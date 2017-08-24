7d895e34679dc40cf8c21ffc856f6c2122fb1949
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2017-08-24 12:40:53 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path              = require('path')
+const webpack           = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const GitRevisionPlugin = require('git-revision-webpack-plugin')
+const gitRevisionPlugin = new GitRevisionPlugin()
+
+module.exports = {
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './src/main.js',
+    // the entry point of our app
+  ],
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules',
+    ],
+    extensions: ['.js', '.json', '.styl'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader',
+      },
+    ]
+
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+
+    new webpack.DefinePlugin({
+      __PROD__       : process.env.NODE_ENV === 'production',
+      __VERSION__    : JSON.stringify(gitRevisionPlugin.version()),
+      __COMMITHASH__ : JSON.stringify(gitRevisionPlugin.commithash()),
+    }),
+
+    new CopyWebpackPlugin([
+      // { from: './assets',     to: path.join(__dirname, '/dist') },
+      { from: '.bin/_redirects', to: path.join(__dirname, '/dist') },
+    ]),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    compress: true,
+    host: 'localhost',
+    port: 3000,
+
+    historyApiFallback: true,
+    // respond to 404s with index.html
+
+    hot: true,
+    // enable HMR on the server
+  },
+}
